bdacaabf1d6ebb7ad9eedd823b96dad591238e90
NicolasMorettiT <99926032+NicolasMorettiT@users.noreply.github.com>
2023-09-22 22:20:26 -0300

    **1.02**
    - Corrigindo o problema 11 do Excel

--- Source-Code-Inspection-master/Source-Code-Inspection-master/Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source-Code-Inspection-master/Source-Code-Inspection-master/Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -37,7 +37,7 @@ public class TicketMachine {
     }
 
     public Iterator<Integer> getTroco() {
-        return null;
+        return new TrocoIterator();
     }
 
     public String imprimir() throws SaldoInsuficienteException {
