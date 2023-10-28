22ea01e2b458ee23b01948bddfe197327abf2a36
kaio Henrique da Silva Souza <42688336+kaio12344321@users.noreply.github.com>
2023-10-27 21:14:48 -0300

    Testes 2
    - Adicionando Teste da Classe Ticket Machine

--- Source-Code-Inspection-master/Source-Code-Inspection-master/Source Code Inspection/src/test/java/br/calebe/ticketmachine/coretest/PapelMoedaTest.java
+++ Source-Code-Inspection-master/Source-Code-Inspection-master/Source Code Inspection/src/test/java/br/calebe/ticketmachine/coretest/PapelMoedaTest.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
 package br.calebe.ticketmachine.coretest;
 
 import org.junit.jupiter.api.*;
@@ -9,11 +13,17 @@ import br.calebe.ticketmachine.core.PapelMoeda;
  * @author Nicolas Moretti/Kaio Henrique
  *
  */
-
 public class PapelMoedaTest {
-    @Test
-    public void testObjectInstance(){
-        PapelMoeda papelMoeda = new PapelMoeda(100, 5);
-        Assertions.assertEquals(100, papelMoeda.getValor());
-    }
+    public void testaValor(){
+            PapelMoeda papelMoeda = new PapelMoeda(100, 5);
+
+            Assertions.assertEquals(100, papelMoeda.getValor());
+        }
+
+        @Test
+        public void testaQuantidade(){
+            PapelMoeda papelMoeda = new PapelMoeda(100, 5);
+
+            Assertions.assertEquals(5, papelMoeda.getQuantidade());
+        }
 }
---
+++ Source-Code-Inspection-master/Source-Code-Inspection-master/Source Code Inspection/src/test/java/br/calebe/ticketmachine/coretest/TicketMachineTest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package br.calebe.ticketmachine.coretest;
+
+import org.junit.jupiter.api.*;
+
+import br.calebe.ticketmachine.core.TicketMachine;
+
+/**
+ *
+ * @author Nicolas Moretti/Kaio Henrique
+ */
+
+public class TicketMachineTest {
+
+    @Test
+    public void testaSaldo(){
+        TicketMachine ticketmachine = new TicketMachine(100);
+
+        Assertions.assertEquals(0, ticketmachine.getSaldo());
+    }
+
+}
